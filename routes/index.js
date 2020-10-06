da2d53db366db0a4f8d2144f7f259e87a8d21435
jieun <love9ly@gmail.com>
2020-10-06 17:31:35 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -4,46 +4,38 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
   res.sendfile(path.join(__dirname, '../public/index.html'));
 });
 
 let server = require('http');
 let svr, port = 12345;
 
-var response = [];
+var response;
 
 svr = server.createServer((req, res) => {
   let str = '';
   res.writeHead(200, { 'X-M2M-RSC': 2000 });
 
   req.on('data', (chunk) => {
-    //console.log(chunk.toString() + '\n');
     str = chunk.toString();
-    response.push(str);
+    response = str;
     console.log(response);
   }).on('end', () => {
     res.end(
       hello(JSON.parse(str))
     )
-    
-
   });
-  
-
 });
+
 svr.listen(port, 'localhost');
 svr.close();
 
-
-router.get('/notify', (req, res) => {
-	//   data = 'testsetse'
+router.get('/notification', (req, res) => {
 	return res.json(response);
 })
 
 function hello(obj) {
   console.log(obj);
-
   return JSON.stringify(obj);
 }
 
